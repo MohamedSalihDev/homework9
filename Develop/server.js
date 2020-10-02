02f2faecb1036f25e0c38e3a63ab6c51accd3996
Mohamed Salih <Mohamed.Salih.Dev@gmail.com>
2020-10-01 19:00:46 -0700

    fixed routes

--- Develop/server.js
+++ Develop/server.js
@@ -3,10 +3,6 @@ const path = require("path");
 const fs = require("fs");
 const bodyParser = require('body-parser');
 
-
-
-// Sets up the Express App
-// =============================================================
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
@@ -14,98 +10,79 @@ app.use(bodyParser.json());
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/notes", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("/api/notes", function (req, res) {
+  fs.readFile(__dirname + "/db/db.json", "utf8", function (error, data) {
+
+    if (error) {
+      return console.log(error);
+    }
+
+    return res.json(data);
   });
-  
-  app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
-  });
-  app.get("/api/notes", function(req, res) {
-    fs.readFile("./db/db.json", "utf8", function(error, data) {
+});
 
-        if (error) {
-          return console.log(error);
-        }
-      
-        //console.log(data);
-      
-        return res.json(data);
-      });
-  });
-  app.post("/api/notes/", function(req, res) {
-    // req.body hosts is equal to the JSON post sent from the user
-    // This works because of our body parsing middleware
-    const newNote = req.body;
-    let id = newNote.id;
-    id = 0;
+app.post("/api/notes/", function (req, res) {
+  const newNote = req.body;
+  fs.readFile(__dirname + "/db/db.json", "utf8", function (error, data) {
+    if (error) {
+      return console.log(error);
+    }
+    const arr = JSON.parse(data)
 
-    id++;
-    fs.readFile("/db/db.json", "utf8", function(error, data) {
-        
-        
-        
-        if (error) {
-          return console.log(error);
-        }
-        data =[];
-        data.push(newNote);
-            
-          }
-        
-      
-      
-        //console.log(data);
-      
-      );
-  
-  
-    // fs.appendFile("./db/db.json", JSON.stringify(newNote) + "\n", function(err) {
-    //     if (err) {
-    //       return console.log(err);
-    //     }
-      
-    //     console.log(newNote);
-      
-    //   });
+    console.log(arr);
+    console.log(newNote)
+    arr.push(newNote);
+    console.log(arr)
+    const stringifiedarr = JSON.stringify(arr)
+    fs.writeFile("./db/db.json", stringifiedarr, function (err) {
+      if (err) {
+        return console.log(err);
+      }
 
-    
-  
-    res.json(newNote);
-  });
+    });
 
-  app.delete("./api/notes/:id", function(req,res){
-      
-      
-      fs.readFile("/db/db.json", "utf8", function(error, data) {
-        const idOfNoteToBeDeleted = req.params.id;
-        
-        
-        if (error) {
-          return console.log(error);
-        }
-        for (let i = 0; i < data.length; i++) {
-            if (idOfNoteToBeDeleted == data[i].id) {
-                data.splice([i],1);
-                return res.json(data);
-            
-          }
-        }
-      
-      
-        //console.log(data);
-      
-      });
-  
-    return res.json(false);
   })
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
-  
 
-  
+});
+
+app.delete("/api/notes/:id", function (req, res) {
+
+
+  fs.readFile(__dirname + "/db/db.json", "utf8", function (error, data) {
+    const idOfNoteToBeDeleted = req.params.id;
+    const parsedData = JSON.parse(data);
+
+    if (error) {
+      return console.log(error);
+    }
+    for (let i = 0; i < parsedData.length; i++) {
+      if (idOfNoteToBeDeleted == parsedData[i].id) {
+
+        parsedData.splice([i], 1);
+
+      }
+    }
+
+  });
+
+  return res.json(false);
+})
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
+
+
+
 
 
 
